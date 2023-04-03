cd352d0e1e5a2828b46310350213625e41806311
Martien Dermawan Tanama <martiendt@gmail.com>
2023-04-03 10:17:36 +0700

    trim data

--- src/modules/auth/controllers/signin.controller.ts
+++ src/modules/auth/controllers/signin.controller.ts
@@ -1,3 +1,4 @@
+import { trim } from "@point-hub/express-utils";
 import { NextFunction, Request, Response } from "express";
 import { validate } from "../request/signin.request.js";
 import { SigninUserService } from "../services/signin.service.js";
@@ -9,7 +10,7 @@ export const signin = async (req: Request, res: Response, next: NextFunction) =>
 
     const signinUserService = new SigninUserService(db);
 
-    const result = await signinUserService.handle(req.body.username, req.body.password);
+    const result = await signinUserService.handle(trim(req.body.username), trim(req.body.password));
 
     res.status(200).json({
       name: result.name,
