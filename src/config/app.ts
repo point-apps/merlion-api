713acf14a54e8d27410bbd4887dfc884bb3672e0
Martien Dermawan Tanama <martiendt@gmail.com>
2023-11-01 16:08:04 +0700

    ok

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,5 +3,5 @@ import { config } from "dotenv";
 config();
 
 export const appName = "pointhub-merlion";
-export const apiUrl = process.env.API_URL || "";
-export const appUrl = process.env.APP_URL || "";
+export const apiUrl = process.env.API_URL || "https://api.merlion.pointhub.app";
+export const appUrl = process.env.APP_URL || "https://merlion.pointhub.app";
