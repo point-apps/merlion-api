3880302f9ed3142e3904d7b63d02125c18f0a988
Martien Dermawan Tanama <martiendt@gmail.com>
2025-08-26 13:41:27 +0700

    fix

--- src/modules/captures/request/create.request.ts
+++ src/modules/captures/request/create.request.ts
@@ -7,10 +7,23 @@ export const validate = (body: any) => {
     activity: "required",
     description: "required",
     observer: "required",
-    "clusters.*.ikigai": "required_if:is_identifiable,true",
+    // "clusters.*.ikigai": "required_if:is_identifiable,true",
     isDraft: "required",
     files: "required",
   });
+
+  // Custom validation for ikigai inside clusters
+  const clusterErrors: Record<string, string[]> = {};
+
+  body.clusters?.forEach((cluster: any, index: number) => {
+    if (cluster?.is_identifiable === true && cluster?.ikigai.length == 0) {
+      clusterErrors[`clusters.${index}.ikigai`] = ["The ikigai field is required when is_identifiable is true."];
+    }
+  });
+
+  if (Object.keys(clusterErrors).length > 0) {
+    throw new ApiError(422, clusterErrors);
+  }
 
   if (validation.fails()) {
     throw new ApiError(422, validation.errors.errors);
